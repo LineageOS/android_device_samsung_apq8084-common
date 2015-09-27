ccf68696a5819408ab6f63d29121404c86498b59
gekkehenkie11 <allard_addink@hotmail.com>
2015-09-27 15:08:21 -0400

    TRLTE: fix MMS data drop
    Change-Id: Id243db44cff6bf39c95e1952ce2e21645a51564c

--- system_prop.mk
+++ system_prop.mk
@@ -33,7 +33,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.netmgrd.qos.enable=true \
     persist.radio.add_power_save=1 \
-    persist.radio.lte_vrat_report=1
+    persist.radio.lte_vrat_report=1 \
+    ro.telephony.mms_data_profile=5
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
