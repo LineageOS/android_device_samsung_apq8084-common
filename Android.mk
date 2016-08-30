2c14c98c0336df9e0ac0f986b9074e6ec33ace52
Say <sktjdgns1189@naver.com>
2016-08-31 05:48:38 +0900

    Android.mk: fix keymaster firmware symlink /firmware/image/skeymaste.*** -> /vendor/firmware/keymaster/keymaste.***

--- Android.mk
+++ Android.mk
@@ -53,12 +53,12 @@ ALL_DEFAULT_INSTALLED_MODULES += $(DTCPIP_SYMLINKS)
 KM_IMAGES := \
     keymaste.b00 keymaste.b01 keymaste.b02 keymaste.b03 keymaste.mdt
 
-KM_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(KM_IMAGES)))
+KM_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/etc/firmware/keymaster/,$(notdir $(KM_IMAGES)))
 $(KM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Keymaster firmware link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+	$(hide) ln -sf /firmware/image/s$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(KM_SYMLINKS)
 
