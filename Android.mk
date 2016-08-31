f0693305574ab5944cb18467e91677226931f8c0
Say <sktjdgns1189@naver.com>
2016-08-31 12:49:16 +0900

    Android.mk: fix keymaster path /vendor/firmware/keymaster is correct, not /vendor/etc/firmware/keymaster

--- Android.mk
+++ Android.mk
@@ -53,7 +53,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(DTCPIP_SYMLINKS)
 KM_IMAGES := \
     keymaste.b00 keymaste.b01 keymaste.b02 keymaste.b03 keymaste.mdt
 
-KM_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/etc/firmware/keymaster/,$(notdir $(KM_IMAGES)))
+KM_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/keymaster/,$(notdir $(KM_IMAGES)))
 $(KM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Keymaster firmware link: $@"
 	@mkdir -p $(dir $@)
