ffe29f554c4da9860003a3bb435e9178d34268c4
Corinna Vinschen <xda@vinschen.de>
2017-03-20 20:44:02 +0100

    camera: CM -> LineageOS
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012-2016, The CyanogenMod Project
+ * Copyright (C) 2017, The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,7 +57,7 @@ camera_module_t HAL_MODULE_INFO_SYM = {
          .hal_api_version = HARDWARE_HAL_API_VERSION,
          .id = CAMERA_HARDWARE_MODULE_ID,
          .name = "APQ8084 Camera Wrapper",
-         .author = "The CyanogenMod Project",
+         .author = "The LineageOS Project",
          .methods = &camera_module_methods,
          .dso = NULL,
          .reserved = {0},
