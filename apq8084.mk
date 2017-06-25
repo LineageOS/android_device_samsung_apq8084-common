de3a08bc78196f9b9b0bc8fc83dc680cd22a78e0
Corinna Vinschen <xda@vinschen.de>
2017-06-25 22:29:13 +0200

    apq8084-common: Add mixer_paths.xml
    Change-Id: Ib15bedff3e88fdfff5cf95f3b9fce62cdbe88b85
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- apq8084.mk
+++ apq8084.mk
@@ -78,7 +78,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Wifi
 PRODUCT_PACKAGES += \
