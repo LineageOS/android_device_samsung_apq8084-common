8a72a4bc0e55eb3c695ea58e9854e4851ee9b3dd
LuK1337 <priv.luk@gmail.com>
2017-09-05 19:53:26 +0200

    apq8084-common: Build our MultiHAL sensors HAL
    Change-Id: I32172a0a6f5b3bf15ead878e91c2edd25b6f1625

--- apq8084.mk
+++ apq8084.mk
@@ -202,9 +202,16 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/_hals.conf:system/vendor/etc/sensors/_hals.conf
+
 # Snap Camera
 #PRODUCT_PACKAGES += \
 #    Snap
+
+PRODUCT_PACKAGES += \
+    sensors.apq8084
 
 # Thermal engine
 PRODUCT_COPY_FILES += \
