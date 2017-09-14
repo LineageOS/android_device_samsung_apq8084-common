4d7a594e8fc9f12d8039cb74f622be824c971285
Ethan Chen <intervigil@gmail.com>
2017-09-14 01:01:28 -0700

    apq8084-common: Add seccomp policy
    Change-Id: I1d5017228a4a9318f8a77ef4036532d64718ecbb

--- apq8084.mk
+++ apq8084.mk
@@ -205,6 +205,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librmnetctl
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+
 # Snap Camera
 #PRODUCT_PACKAGES += \
 #    Snap
