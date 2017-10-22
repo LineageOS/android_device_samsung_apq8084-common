17e572e9d241705c213a51f186d8edcd9202e2ec
Corinna Vinschen <xda@vinschen.de>
2017-10-22 14:30:49 +0200

    apq8084-common: Move bdroid_buildcfg.h here from kccat6/lentislte
    Only keep BTM_DEF_LOCAL_NAME in device-specific header btm_localname.h.
    
    Change-Id: I0c4becb17d09b4496792e533c2cb3080739b5f8a
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

---
+++ bluetooth/bdroid_buildcfg.h
@@ -0,0 +1,42 @@
+/*
+ *
+ *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ *  Not a Contribution, Apache license notifications and license are retained
+ *  for attribution purposes only.
+ *
+ * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2016 The Sayanogen Project
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+#include "btm_localname.h"
+// Disables read remote device feature
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
+#define MAX_ACL_CONNECTIONS    16
+#define MAX_L2CAP_CHANNELS    16
+// skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD  TRUE
+
+#define BLE_VND_INCLUDED   FALSE
+#define BLE_PRIVACY_SPT    FALSE
+#define BT_CLEAN_TURN_ON_DISABLED 1
+#define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
+#define BTIF_HF_WBS_PREFERRED TRUE  /* Use WBS    */
+/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
+#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
+#endif
+
