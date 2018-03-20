e6549c99b1361aecccebeff792687b965112a183
Corinna Vinschen <xda@vinschen.de>
2018-03-20 17:35:05 +0100

    apq8084-common: add symlinks to camera vendor files
    Change-Id: Ib325d34533a675a5604157712afda5f69afbc3fe
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- Android.mk
+++ Android.mk
@@ -237,3 +237,133 @@ $(SEC_BIN_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	$(hide) ln -sf /system/vendor/bin/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(SEC_BIN_SYMLINKS)
+
+include $(CLEAR_VARS)
+CAMVERS_FILES := \
+    F16UL_s5k2p2xx_module_info.xml F16US_imx240_module_info.xml
+
+CAMVERS_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/,$(notdir $(CAMVERS_FILES)))
+$(CAMVERS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Camera module version link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/vendor/etc/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(CAMVERS_SYMLINKS)
+
+include $(CLEAR_VARS)
+CAMDATA_SYMLINK := $(TARGET_OUT)/cameradata
+$(CAMDATA_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Cameradata symlink: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/vendor/cameradata $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(CAMDATA_SYMLINK)
+
+include $(CLEAR_VARS)
+CAMLIB_FILES := \
+    F16UL_libchromatix_s5k2p2xx_default_video.so \
+    F16UL_libchromatix_s5k2p2xx_golfshot.so \
+    F16UL_libchromatix_s5k2p2xx_golfshot_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_hdr_liveshot_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_hdr_preview.so \
+    F16UL_libchromatix_s5k2p2xx_hdr_preview_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_hdr_preview_lls_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_hdr_uhd_video.so \
+    F16UL_libchromatix_s5k2p2xx_hdr_uhd_video_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_hdr_video.so \
+    F16UL_libchromatix_s5k2p2xx_hdr_video_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_hdr_zslshot_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_hdr_zslshot_lls_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_hfr_1080p_b.so \
+    F16UL_libchromatix_s5k2p2xx_hfr_1080p_b_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_hfr_120.so \
+    F16UL_libchromatix_s5k2p2xx_hfr_120_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_liveshot_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_pip.so \
+    F16UL_libchromatix_s5k2p2xx_preview.so \
+    F16UL_libchromatix_s5k2p2xx_preview_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_preview_drama_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_preview_lls_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_preview_panorama_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_preview_pip_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_shotmode_preview.so \
+    F16UL_libchromatix_s5k2p2xx_shotmode_preview_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_shotmode_zslshot_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_uhd_video.so \
+    F16UL_libchromatix_s5k2p2xx_uhd_video_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_video_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_zslshot_2_4_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_zslshot_2_4_iso100_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_zslshot_2_4_iso200_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_zslshot_2_4_iso400_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_zslshot_2_4_iso800_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_zslshot_2_4_lls_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_zslshot_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_zslshot_iso100_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_zslshot_iso200_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_zslshot_iso400_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_zslshot_iso800_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_zslshot_lls_cpp.so \
+    F16UL_libchromatix_s5k2p2xx_zslshot_pip_cpp.so \
+    F16UL_libTsAe.so F16UL_libTsAf.so \
+    F16UL_libTs_J_Accm.so F16UL_libTs_J_Awb.so \
+    F16US_libchromatix_imx240_default_video.so \
+    F16US_libchromatix_imx240_golfshot.so \
+    F16US_libchromatix_imx240_golfshot_cpp.so \
+    F16US_libchromatix_imx240_hdr_liveshot_cpp.so \
+    F16US_libchromatix_imx240_hdr_preview.so \
+    F16US_libchromatix_imx240_hdr_preview_cpp.so \
+    F16US_libchromatix_imx240_hdr_preview_lls_cpp.so \
+    F16US_libchromatix_imx240_hdr_uhd_video.so \
+    F16US_libchromatix_imx240_hdr_uhd_video_cpp.so \
+    F16US_libchromatix_imx240_hdr_video.so \
+    F16US_libchromatix_imx240_hdr_video_cpp.so \
+    F16US_libchromatix_imx240_hdr_zslshot_cpp.so \
+    F16US_libchromatix_imx240_hdr_zslshot_lls_cpp.so \
+    F16US_libchromatix_imx240_hfr_1080p_b.so \
+    F16US_libchromatix_imx240_hfr_1080p_b_cpp.so \
+    F16US_libchromatix_imx240_hfr_120.so \
+    F16US_libchromatix_imx240_hfr_120_cpp.so \
+    F16US_libchromatix_imx240_liveshot_cpp.so \
+    F16US_libchromatix_imx240_pip.so \
+    F16US_libchromatix_imx240_preview.so \
+    F16US_libchromatix_imx240_preview_cpp.so \
+    F16US_libchromatix_imx240_preview_drama_cpp.so \
+    F16US_libchromatix_imx240_preview_lls_cpp.so \
+    F16US_libchromatix_imx240_preview_panorama_cpp.so \
+    F16US_libchromatix_imx240_preview_pip_cpp.so \
+    F16US_libchromatix_imx240_shotmode_preview.so \
+    F16US_libchromatix_imx240_shotmode_preview_cpp.so \
+    F16US_libchromatix_imx240_shotmode_zslshot_cpp.so \
+    F16US_libchromatix_imx240_uhd_video.so \
+    F16US_libchromatix_imx240_uhd_video_cpp.so \
+    F16US_libchromatix_imx240_video_cpp.so \
+    F16US_libchromatix_imx240_zslshot_2_4_cpp.so \
+    F16US_libchromatix_imx240_zslshot_2_4_iso100_cpp.so \
+    F16US_libchromatix_imx240_zslshot_2_4_iso200_cpp.so \
+    F16US_libchromatix_imx240_zslshot_2_4_iso400_cpp.so \
+    F16US_libchromatix_imx240_zslshot_2_4_iso800_cpp.so \
+    F16US_libchromatix_imx240_zslshot_2_4_lls_cpp.so \
+    F16US_libchromatix_imx240_zslshot_cpp.so \
+    F16US_libchromatix_imx240_zslshot_iso100_cpp.so \
+    F16US_libchromatix_imx240_zslshot_iso200_cpp.so \
+    F16US_libchromatix_imx240_zslshot_iso400_cpp.so \
+    F16US_libchromatix_imx240_zslshot_iso800_cpp.so \
+    F16US_libchromatix_imx240_zslshot_lls_cpp.so \
+    F16US_libchromatix_imx240_zslshot_pip_cpp.so \
+    F16US_libTsAe.so F16US_libTsAf.so \
+    F16US_libTs_J_Accm.so F16US_libTs_J_Awb.so \
+    libmmcamera_cac3_lib.so libmmcamera_fidelix_eeprom.so \
+    libmmcamera_interface.so libmmcamera2_stats_algorithm.so \
+    libTsAe.so libTsAf.so libTs_J_Accm.so libTs_J_Awb.so
+
+CAMLIB_SYMLINKS := $(addprefix $(TARGET_OUT)/lib/,$(notdir $(CAMLIB_FILES)))
+$(CAMLIB_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Camera module version link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/vendor/lib/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(CAMLIB_SYMLINKS)
