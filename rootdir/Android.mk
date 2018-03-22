b70860fb9a6e70977bc617efa1478f5115a406a9
Stefan Assmann <sassmann@kpanic.de>
2018-03-22 16:59:24 +0100

    apq8084-common: clean-up rc files
    - Add LOCAL_MODULE_PATH for init.trace.rc
    - wcnss_service is not needed
    - netmgrd does not exist
    - hostapd is started by /vendor/etc/init/hostapd.android.rc
    - groups qcom_diag, vibe no longer exist
    - /dev/radio0 does not exist
    
    Change-Id: Ie089b055e1ca85d610c6fa622ab93f91394f5ba4

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -68,6 +68,7 @@ LOCAL_MODULE       := init.trace.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.trace.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 BOARD_ROOT_EXTRA_SYMLINKS := /data/ssh:var/run/ssh
