502d88c04becbae5185f3bef44b655f5e67bcb66
Corinna Vinschen <xda@vinschen.de>
2018-03-30 14:28:58 +0200

    apq8084-common: Move GPS configs from proprietary here.
    Add missing izat.conf from CQI4.
    
    Change-Id: Id046bd345382cb8d0a4e343d3ba89455a7597fe6

--- apq8084.mk
+++ apq8084.mk
@@ -117,6 +117,13 @@ PRODUCT_PACKAGES += \
     ebtables \
     ethertypes
 
+# GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/flp.conf:/system/etc/flp.conf \
+    $(LOCAL_PATH)/configs/gps.conf:/system/etc/gps.conf \
+    $(LOCAL_PATH)/configs/izat.conf:/system/etc/izat.conf \
+    $(LOCAL_PATH)/configs/sap.conf:/system/etc/sap.conf
+
 # IR Blaster
 PRODUCT_PACKAGES += \
     consumerir.apq8084
