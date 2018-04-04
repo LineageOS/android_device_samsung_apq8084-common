fc5950940e52df1bfcac6057dd0a29fe954179bb
Corinna Vinschen <xda@vinschen.de>
2018-04-04 16:01:24 +0200

    Revert "apq8084-common: fix android.hardware.wifi@1.0-service capabilities"
    This reverts commit 46e4452bfa02f1414394b6e866176fc9dcd93845.
    
    wlan module is builtin now, so this isn't required anymore.
    
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- config.fs
+++ config.fs
@@ -6,9 +6,3 @@ value:2951
 
 [AID_RFS_SHARED]
 value:2952
-
-[system/vendor/bin/hw/android.hardware.wifi@1.0-service]
-mode: 0755
-user: AID_WIFI
-group: AID_WIFI
-caps: NET_ADMIN NET_RAW SYS_MODULE
