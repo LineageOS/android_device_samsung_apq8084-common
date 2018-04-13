7ac75d117bb3bfd2acb302aa2d5e70f886581cd0
Corinna Vinschen <xda@vinschen.de>
2018-04-13 13:36:32 +0200

    apq8084-common: Accommodate ResurrectionRemix build
    - Don't define AID_QCOM_DIAG, it is already defined in common header
      system/core/libcutils/include/private/android_filesystem_config.h
    
    - Add Jelly to PRODUCT_PACKAGES to add a default browser.
    
    Change-Id: Ie7f7b6764262897092b9491f5e3d388118d4425a
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- apq8084.mk
+++ apq8084.mk
@@ -90,6 +90,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/vendor/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/vendor/etc/usb_audio_policy_configuration.xml
 
+# Browser
+PRODUCT_PACKAGES += \
+    Jelly
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.apq8084 \
