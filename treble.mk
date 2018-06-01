b3fcb10dbc3620b73cda7c0cd8718da8f64ce901
Bruno Martins <bgcngm@gmail.com>
2018-06-01 20:53:33 +0200

    apq8084-common: Include DRM service
    Change-Id: I76eccf8e0741fb7f756508f1f9cffafc3c8b7891
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- treble.mk
+++ treble.mk
@@ -31,7 +31,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service
