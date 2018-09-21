79a7b90b03073f7779f51766286492233d839971
Kevin F. Haggerty <haggertk@lineageos.org>
2018-09-20 21:54:42 -0600

    apq8084-common: Build binderized samsung8084 sensors service
    [sassmann]: picked from android_device_samsung_msm8974-common
    
    Change-Id: I85010c4ab26c771c585e0dc65d14552853f147c2

--- treble.mk
+++ treble.mk
@@ -72,7 +72,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+    android.hardware.sensors@1.0-impl.apq8084 \
+    android.hardware.sensors@1.0-service.apq8084
 
 # USB
 PRODUCT_PACKAGES += \
